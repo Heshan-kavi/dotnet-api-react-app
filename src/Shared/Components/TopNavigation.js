990a33fb98573f37a89a6d9a40b8c7825d7c711d
heshankavinda <2017cs083@stu.ucsc.cmb.ac.lk>
2023-10-20 13:11:51 +0530

    added things related to navigation

---
+++ src/Shared/Components/TopNavigation.js
@@ -0,0 +1,27 @@
+import { styled } from "styled-components";
+
+
+export const AnchorTag = styled.a`
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 16px 18px;
+    text-decoration: none;
+    font-size: 17px;
+    &:hover,
+    &:focus {
+        color: #BF4F74;
+        background-color: white;
+    }
+    &:active {
+        color: #BF4F74;
+        background-color: white;
+    }
+`;
+
+export const TopNavigation = styled.div`
+    background-color: #BF4F74;
+    overflow: hidden;
+`;
+
+
