7814eaaa94b11673283f0d5b23bc2dca33b95bcf
heshankavinda <2017cs083@stu.ucsc.cmb.ac.lk>
2023-10-26 07:22:11 +0530

    implemented cards

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,13 +1,34 @@
-import React from "react";
 import Background from "../Shared/Components/BackGround";
-import { H1 } from "../Shared/Components/TextContent";
+import { CardContainer, CardItem, CardSub, CardMoneyContainer, CardTitle } from "../Shared/Components/Card";
 import Theme from "../Shared/Components/Theme";
+
+const featuredData = [
+    {title: "Revenue", money: "$3,300", moneyRate: "+12.4"},
+    {title: "Sales", money: "$5,300", moneyRate: "+5.6"},
+    {title: "Cost", money: "$2,000", moneyRate: "+4.7"},
+    {title: "Sales", money: "$5,300", moneyRate: "+5.6"}
+]
 
 export default function DashBoard (props){
 
     const currentTheme = props.themeToUse;
     
     return (
-        <Theme theme={currentTheme}><Background><H1>this is the dashboard page</H1></Background></Theme>
+        <Theme theme={currentTheme}>
+            <Background>
+                <CardContainer>
+                    {featuredData && featuredData.map((item, index) => (
+                        <CardItem key={index}>
+                            <CardTitle>{item.title}</CardTitle>
+                            <CardMoneyContainer>
+                                <span className="featuredMoney">{item.money}</span>
+                                <span className="featuredMoneyRate">{item.moneyRate}</span>
+                            </CardMoneyContainer>
+                            <CardSub>this is the new rates</CardSub>
+                        </CardItem>
+                    ))}
+                </CardContainer>
+            </Background>
+        </Theme>
     )
 }
